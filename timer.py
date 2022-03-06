0fbf21a3d1706d43cb281732ce465d1391b8308b
Woody Lin <jj7158761@gmail.com>
2022-03-07 00:29:12 +0800

    first commit

---
+++ timer.py
@@ -0,0 +1,12 @@
+import time
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        value = func(*args, **kwargs)
+        end_time = time.time()
+        print("{} run time: {}".format(func.__name__, end_time - start_time))
+        return value
+
+    return wrapper
