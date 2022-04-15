d9d98d8794606f9df064732b0a2c268c6d1e7742
WoodyLin <jj7158761@gmail.com>
2022-04-15 18:04:24 +0800

    6-2 Tmp

---
+++ 6-2.py
@@ -0,0 +1,56 @@
+"""
+Find First and Last Position of Element in Sorted Array
+
+Solution
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+
+Example 3:
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+
+Constraints:
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+"""
+from utils.timer import timer
+
+
+class Solution:
+    @timer
+    def searchRange(self, nums: list, target: int) -> list:
+        start, end = -1, -1
+        try:
+            start = nums.index(target)
+            end = len(nums) - 1 - nums[::-1].index(target)
+        except:
+            pass
+        return [start, end]
+
+
+# nums = [5,7,7,8,8,10]
+# target = 8
+# Output: [3,4]
+# nums = [5,7,7,8,8,10]
+# target = 6
+# Output: [-1,-1]
+nums = []
+target = 0
+# Output: [-1,-1]
+
+S = Solution()
+S.searchRange(nums, target)
